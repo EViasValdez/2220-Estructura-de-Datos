bc9b025b5c884690d9654b7a2e947b70b7a202cd
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2022-08-17 22:28:58 -0500

    Add files via upload

---
+++ Piramide/src/piramide/Piramide.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package piramide;
+
+/**
+ *
+ * @author EViasValdez
+ */
+public class Piramide {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int uno = 100000;
+            
+        for (int x = uno; x <= uno; x++) {
+            for (int y = 2; x <= uno; y++) {
+                System.out.println("*");
+                System.out.println("*");
+            }
+        }
+    }
+}
