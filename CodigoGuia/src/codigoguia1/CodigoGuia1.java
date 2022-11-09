0c7e4f3b174ce9a4d51e9a1b47245cb947f3dc64
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2022-11-09 10:43:12 -0600

    Add files via upload

--- CodigoGuia/src/codigoguia1/CodigoGuia1.java
+++ CodigoGuia/src/codigoguia1/CodigoGuia1.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package codigoguia;
+package codigoguia1;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,7 +13,7 @@ import java.io.InputStreamReader;
  *
  * @author EViasValdez
  */
-public class CodigoGuia {
+public class CodigoGuia1 {
 
     /**
      * @param args the command line arguments
