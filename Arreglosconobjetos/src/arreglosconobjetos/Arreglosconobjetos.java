5eb2540b1ef97d6249b040aaa93ca0ed2f5cab7c
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2023-07-07 09:49:01 -0600

    Add files via upload

--- Arreglosconobjetos/src/arreglosconobjetos/Arreglosconobjetos.java
+++ Arreglosconobjetos/src/arreglosconobjetos/Arreglosconobjetos.java
@@ -19,7 +19,7 @@ public class ArreglosconObjetos {
         // TODO code application logic here
         Scanner leer = new Scanner(System.in);
 
-        Persona [] Pacientes = new Persona[4]; // Se declara un arreglo de objetos llamado "Pacientes"
+        Persona[] Pacientes = new Persona[4]; // Se declara un arreglo de objetos llamado "Pacientes"
         // el cual esta instanciado a la Clase "Persona" de tamaño 4.
         // Cada posición del arreglo contendrá un objeto de la clase "Persona" en la posición 0
         // del arreglo se almacenara el Nombre, Edad y Peso de una persona.
@@ -67,7 +67,7 @@ public class ArreglosconObjetos {
             case 2:
                 System.out.println("++++++++++++++ Mostrar el Paciente mayor de Edad ++++++++++++++");
                 Persona PacienteMayor;  // Crea un objeto llamado "PacienteMayor" de tipo clase "Persona"
-                PacienteMayor = Pacientes [0]; // se asigna el objeto de la poción 0 del arreglo "Pacientes" al objeto PacienteMayor
+                PacienteMayor = Pacientes[0]; // se asigna el objeto de la poción 0 del arreglo "Pacientes" al objeto PacienteMayor
 
                 // int i = 1;
                 for (int i = 0; i < Pacientes.length; i++) // Se recorre el arreglo "Pacientes".
